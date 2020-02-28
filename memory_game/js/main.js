5ca6655febc5dc48e393f902982799da200be6d8
Ben <ben.h.poh@gmail.com>
2020-02-28 22:08:12 +1100

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,6 +1,17 @@
-let cardOne = "Queen"
-let cardTwo = "Queen"
-let cardThree = "King"
-let cardFour = "King"
+let cards = ["Queen", "Queen", "King", "King"];
+let cardsInPlay = [];
+let cardOne = cards[0];
+let cardTwo = cards[1];
+
+cardsInPlay.push(cardOne);
+cardsInPlay.push(cardTwo);
 console.log("User flipped " + cardOne);
 console.log("User flipped " + cardTwo);
+
+if (cardsInPlay.length === 2) {
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+		alert("You found a match!");
+	}
+	else { alert("Please try again");
+	}
+}
